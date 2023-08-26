495724cb19615d90bc223500ca7a6f0a6f2e8378
Kewin Rodrigues Veiga Pinto <133122830+GhostSays@users.noreply.github.com>
2023-08-26 14:26:10 -0300

    Create graph_theory.py

---
+++ graph_theory.py
@@ -0,0 +1,33 @@
+def bfs(graph, start, goal):
+    visited = set()
+    queue = []
+
+    queue.append(start)
+    visited.add(start)
+
+    parent_node = dict()
+    parent_node[start] = None
+
+    path_found = False
+    while queue:
+        current_node = queue[0]
+        if current_node == goal:
+            path_found = True
+            break
+        
+        for node in graph[current_node]:
+            if node not in visited:
+                queue.append(node)
+                visited.add(node)
+                parent_node[node] = current_node
+        queue.pop(0) 
+    
+    if path_found:
+        path = list()
+        current_node = goal
+        path.append(current_node)
+        while current_node != start:
+            path.append(parent_node[current_node])
+            current_node = parent_node[current_node]
+        path.reverse()
+        return path
